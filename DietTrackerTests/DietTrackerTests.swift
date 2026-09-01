29fc76c91680b31d519da88e4bd1e0e639c7899a
Aaron <aaronhuxley07@gmail.com>
2026-09-01 18:48:45 +0100

    Initial Commit

---
+++ DietTrackerTests/DietTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  DietTrackerTests.swift
+//  DietTrackerTests
+//
+//  Created by Aaron Huxley on 01/09/2026.
+//
+
+import Testing
+@testable import DietTracker
+
+struct DietTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
