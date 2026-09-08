6e2e4e0894665238c9f6106653e8efa3854f3926
Aaron <aaronhuxley07@gmail.com>
2026-09-08 17:16:20 +0100

    Revamp food entry UI

--- DietTracker/Features/Food/Enums/NutritionUnit.swift
+++ DietTracker/Features/Food/Enums/NutritionUnit.swift
@@ -12,4 +12,13 @@ enum NutritionUnit: String, CaseIterable, Identifiable {
     case per100ml = "Per 100ml"
 
     var id: Self { self }
+
+    var servingSize: String {
+        switch self {
+        case .per100g:
+            "100g"
+        case .per100ml:
+            "100ml"
+        }
+    }
 }
--- DietTracker/Features/Food/Views/FoodEntryView.swift
+++ DietTracker/Features/Food/Views/FoodEntryView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct FoodEntryView: View {
 
-    var food: Food
+    let food: Food
     let existingFoodEntry: FoodEntry?
 
     var onSave: (FoodEntry) -> Void
@@ -39,95 +39,168 @@ struct FoodEntryView: View {
     var body: some View {
         Form {
             foodInformation
-
-            Section("Date") {
-                DatePicker(
-                    "Date",
-                    selection: $foodEntryVM.date,
-                    displayedComponents: .date
-                )
-            }
-
-            Section("Amount") {
-                TextField(
-                    "",
-                    value: $foodEntryVM.amount,
-                    format: .number
-                )
-                .keyboardType(.decimalPad)
-            }
-            
-            Section("Meal") {
-                Picker("Meal", selection: $foodEntryVM.mealType) {
-                    Text("Select a Meal")
-                        .tag(nil as MealType?)
-
-                    ForEach(MealType.allCases) { mealType in
-                        Text(mealType.rawValue.capitalized)
-                            .tag(mealType as MealType?)
-                    }
-                }
-            }
-
-
-            Section {
-                if let existingFoodEntry {
-                    Button("Save Changes") {
-                        let updatedFoodEntry =
-                            foodEntryVM.editFoodEntry(existingFoodEntry)
-
-                        onSave(updatedFoodEntry)
-                    }
-
-                    Button("Delete Food Entry", role: .destructive) {
-                        onDelete(existingFoodEntry)
-                    }
-                } else {
-                    Button("Save") {
-                        let foodEntry = foodEntryVM.createFoodEntry()
-                        onSave(foodEntry)
-                    }
-                }
-            }
+            entryInformation
         }
         .navigationTitle(
             existingFoodEntry == nil
                 ? "Add Food Entry"
                 : "Edit Food Entry"
         )
-    }
-}
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                if let existingFoodEntry {
+                    Button("Log") {
+                        saveChanges(existingFoodEntry)
+                    }
 
-extension FoodEntryView {
-    @ViewBuilder
-    var foodInformation: some View {
-        Section("Name") {
-            Text(food.name)
-        }
-
-        if let brand = food.brand {
-            Section("Brand") {
-                Text(brand)
+                    Button(
+                        "Delete Food Entry",
+                        role: .destructive
+                    ) {
+                        onDelete(existingFoodEntry)
+                    }
+                } else {
+                    Button("Log") {
+                        saveNewEntry()
+                    }
+                }
             }
         }
+    }
+}
 
-        Section("Calories") {
-            Text(String(food.calories))
+// MARK: - Food Information
+
+extension FoodEntryView {
+
+    private var foodInformation: some View {
+        Section {
+            VStack(spacing: 15) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(food.name)
+
+                        if let brand = food.brand {
+                            Text(brand)
+                                .font(.subheadline)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+
+                    Spacer()
+                }
+
+                HStack {
+                    nutritionValue(
+                        value: food.calories,
+                        label: "Calories"
+                    )
+
+                    Spacer()
+
+                    nutritionValue(
+                        value: food.protein,
+                        label: "Protein"
+                    )
+
+                    Spacer()
+
+                    nutritionValue(
+                        value: food.carbohydrates,
+                        label: "Carbs"
+                    )
+
+                    Spacer()
+
+                    nutritionValue(
+                        value: food.fat,
+                        label: "Fat"
+                    )
+                }
+            }
         }
+    }
 
-        Section("Protein (g)") {
-            Text(String(food.protein))
-        }
+    private func nutritionValue(
+        value: Double,
+        label: String
+    ) -> some View {
+        VStack {
+            Text(String(value))
 
-        Section("Carbohydrates (g)") {
-            Text(String(food.carbohydrates))
-        }
-
-        Section("Fat (g)") {
-            Text(String(food.fat))
+            Text(label)
+                .font(.caption)
+                .foregroundStyle(.secondary)
         }
     }
 }
+
+// MARK: - Entry Information
+
+extension FoodEntryView {
+
+    private var entryInformation: some View {
+        Section {
+            HStack {
+                Text("Serving Size")
+
+                Spacer()
+
+                Text(food.nutritionUnit.servingSize)
+            }
+
+            HStack {
+                Text("Servings")
+
+                Spacer()
+
+                TextField(
+                    "",
+                    value: $foodEntryVM.amount,
+                    format: .number
+                )
+                .keyboardType(.decimalPad)
+                .multilineTextAlignment(.trailing)
+            }
+
+            Picker(
+                "Meal",
+                selection: $foodEntryVM.mealType
+            ) {
+                Text("Select a Meal")
+                    .tag(nil as MealType?)
+
+                ForEach(MealType.allCases) { mealType in
+                    Text(mealType.rawValue.capitalized)
+                        .tag(mealType as MealType?)
+                }
+            }
+
+            DatePicker(
+                "Date",
+                selection: $foodEntryVM.date,
+                displayedComponents: .date
+            )
+        }
+    }
+}
+
+// MARK: - Actions
+
+extension FoodEntryView {
+
+    private func saveNewEntry() {
+        let foodEntry = foodEntryVM.createFoodEntry()
+        onSave(foodEntry)
+    }
+
+    private func saveChanges(_ foodEntry: FoodEntry) {
+        let updatedFoodEntry = foodEntryVM.editFoodEntry(foodEntry)
+        onSave(updatedFoodEntry)
+    }
+}
+
+// MARK: - Preview
 
 #Preview("Add Food Entry") {
     FoodEntryView(
